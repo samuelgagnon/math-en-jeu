01ce19773e59a3eb3bbede0067a5e210728e8780
sylvainjambon <sylvainjambon@00d195c3-d43a-0410-8499-09eb43983aed>
2005-11-06 17:06:33 +0000

    Rien à déclarer

---
+++ serveur/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<!--
+  Modele de page du jeu Math en jeu
+  Projet:         Math en jeu
+  Auteur:         Sylvain Halle <sylvain.halle@smac.ulaval.ca>
+  Derniere modif: 2005/08/12
+  Modifie par:    Sylvain Halle
+-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css" href="mathenjeu.css">
+<meta name="lang" content="fr">
+<link rel="SHORTCUT ICON" href="smac-icone.ico">
+<meta name="robots" content="noindex,nofollow">
+<meta name="author" content="Sylvain Hall&eacute;">
+<meta name="description" content="Page du jeu Math en jeu">
+<meta name="keywords" content="jeu, mathematiques, sciences, jeunes, education, secondaire, math en jeu">
+<title>
+Math en jeu!
+</title>
+</head>
+<body style="margin:0px;width:100%;background-image:url('/page-text-tiled.jpg');">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="background-image:url('nothing.gif');">
+<tr>
+<!--
+<td style="background-image:url('haut-gauche-coin.jpg');" width="31">
+<img src="nothing.gif" alt="" width="31">
+</td>
+-->
+<td style="background-image:url('haut-gauche-stretch.jpg');">
+<img src="nothing.gif" alt="">
+</td>
+<td width="700">
+<img src="haut-centre.jpg" alt="Math en jeu" width="700">
+</td>
+<td style="background-image:url('haut-droite-stretch.jpg');">
+<img src="nothing.gif" alt="">
+</td>
+<!-- 
+<td style="background-image:url('haut-droite-coin.jpg');" width="40">
+<img src="nothing.gif" alt="" width="40">
+</td>
+-->
+</tr>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<tr>
+<td style="background-image:url('nothing.gif');" width="10">&nbsp;</td>
+<td style="background-image:url('nothing.gif');">
+<!-- Début contenu page -->
+<h1>Titre de la page</h1>
+<!-- Fin contenu page -->
+<!-- Début footer -->
+<hr>
+<center>
+&copy; 2005 <a href="http://www.ulaval.ca/">Université Laval</a> Tous droits réservés
+</center>
+<!-- Fin footer -->
+</td>
+<td style="background-image:url('nothing.gif');" width="10">&nbsp;</td>
+</table>
+
+</body>
+</html>
