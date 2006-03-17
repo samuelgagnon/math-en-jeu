28f084baaebfcf9755fb1878cdb2e976e4105428
dumoulinm <dumoulinm@00d195c3-d43a-0410-8499-09eb43983aed>
2006-03-17 12:05:22 +0000

    Ajout evenement pour le temps et fin de partie

---
+++ serveur/ServeurJeu/Evenements/EvenementPartieTerminee.java
@@ -0,0 +1,73 @@
+/*
+ * Created on 2006-03-17
+ *
+ * 
+ */
+package ServeurJeu.Evenements;
+
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+import ClassesUtilitaires.UtilitaireXML;
+
+/**
+ * @author Marc
+ *
+ * 
+ */
+public class EvenementPartieTerminee  extends Evenement
+{
+	protected String genererCodeXML(InformationDestination information)
+	{
+	    // Déclaration d'une variable qui va contenir le code XML à retourner
+	    String strCodeXML = "";
+	    
+		try
+		{
+	        // Appeler une fonction qui va créer un document XML dans lequel 
+		    // on peut ajouter des noeuds
+	        Document objDocumentXML = UtilitaireXML.obtenirDocumentXML();
+
+			// Créer le noeud de commande à retourner
+			Element objNoeudCommande = objDocumentXML.createElement("commande");
+			
+			// Créer le noeud du paramètre
+			Element objNoeudParametre = objDocumentXML.createElement("parametre");
+			
+			// Créer un noeud contenant le nom d'utilisateur du noeud paramètre
+			
+			// Définir les attributs du noeud de commande
+			objNoeudCommande.setAttribute("no", Integer.toString(information.obtenirNoCommande()));
+			objNoeudCommande.setAttribute("type", "Evenement");
+			objNoeudCommande.setAttribute("nom", "PartieTerminee");
+			
+			// On ajoute un attribut type qui va contenir le type
+			// du paramètre
+			
+			// Ajouter le noeud texte au noeud du paramètre
+			
+			// Ajouter le noeud paramètre au noeud de commande
+			objNoeudCommande.appendChild(objNoeudParametre);
+			
+			// Ajouter le noeud de commande au noeud racine dans le document
+			objDocumentXML.appendChild(objNoeudCommande);
+
+			// Transformer le document XML en code XML
+			strCodeXML = UtilitaireXML.transformerDocumentXMLEnString(objDocumentXML);
+		}
+		catch (TransformerConfigurationException tce)
+		{
+			System.out.println("Une erreur est survenue lors de la transformation du document XML en chaine de caracteres");
+		}
+		catch (TransformerException te)
+		{
+			System.out.println("Une erreur est survenue lors de la conversion du document XML en chaine de caracteres");
+		}
+		
+		return strCodeXML;
+	}
+}
