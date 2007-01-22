f9beb6656b0fea2834a2806213911123beec4306
maximeb <maximeb@00d195c3-d43a-0410-8499-09eb43983aed>
2007-01-22 15:21:52 +0000

    ajout du code pour afficher un message d'installation pour le client flash s'il n'est pas installé.

--- mathenjeu-www/jeu-popup.php
+++ mathenjeu-www/jeu-popup.php
@@ -21,6 +21,8 @@ if (!isset($_SESSION["joueur"]))
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" style="margin:0px;padding:0px;height:100%;">
 <head>
+<meta http-equiv="Expires" content="Thu, 01 Dec 1994 16:00:00 GMT"/>
+<meta http-equiv="Cache-Control" content="no-cache, max-age=0"/>
 <script type="text/javascript">
 <!-- Begin
 function Launch(page) {
@@ -33,7 +35,25 @@ function Launch(page) {
 </head>
 <body bgcolor="#000000" style="margin:0px;padding:0px;height:100%;width:100%;">
 <div style="height:100%">
+
+<OBJECT 
+	classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+	codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"
+	WIDTH="100%" HEIGHT="100%" id="mathenjeu">
+	<PARAM NAME=movie VALUE="flash/chargement_beta.swf?nomUtilisateur=<?php echo $_GET["alias"]; ?>&amp;motDePasse=<?php echo $_GET["motDePasse"]; ?>">
+	<PARAM NAME=quality VALUE=high>
+	<param name="bgcolor" value="#000000" />
+	<param name="allowFullScreen" value="true" />
+	<EMBED src="flash/chargement_beta.swf?nomUtilisateur=<?php echo $_GET["alias"]; ?>&amp;motDePasse=<?php echo $_GET["motDePasse"]; ?>" 
+		quality=high bgcolor=#FFFFFF WIDTH="100%" HEIGHT="100%"
+		NAME="mathenjeu" TYPE="application/x-shockwave-flash"
+		PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer">
+	</EMBED>
+</OBJECT>
+
+<!--
 <object
+		codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0"
         type="application/x-shockwave-flash"
         data="flash/chargement_beta.swf?nomUtilisateur=<?php echo $_GET["alias"]; ?>&amp;motDePasse=<?php echo $_GET["motDePasse"]; ?>"
         width="100%"
@@ -42,8 +62,12 @@ function Launch(page) {
     	<param name="allowScriptAccess" value="always" />
 		<param name="quality" value="high" />
 		<param name="bgcolor" value="#000000" />
-		<!--<param name="scale" value="exactfit" />-->
+		<EMBED WIDTH="550" HEIGHT="400"
+			TYPE="application/x-shockwave-flash"
+			pluginspage="http://www.macromedia.com/go/getflashplayer"></EMBED>
+
 </object>
+-->
 </div>
 </body>
 </html>
