698f01d0dffc677e59f59eb5f57a0d5e3acf764b
maxime.begin <maxime.begin@00d195c3-d43a-0410-8499-09eb43983aed>
2007-10-30 00:58:01 +0000

    Issue : MEJ-23, MEJ-32
    Minor modification to some primary key. Added data to the test_data.

--- database/test/test_data.sql
+++ database/test/test_data.sql
@@ -2,7 +2,9 @@ truncate table user;
 truncate table school;
 truncate table school_type;
 truncate table role;
-
+truncate table subject_level;
+truncate table subject;
+truncate table level;
 truncate table language; 
 insert into language(name, short_name) values('français','fr');
 insert into language(name, short_name) values('english','en');
@@ -45,3 +47,45 @@ insert into role(tag) values('SIMPLE_USER');
 
 insert into user (name, last_name, school_id, sexe, username, password, city, country, email, inscription_date, province, email_confirmation_key, role_id)
 	values ('Maxime', 'Bégin', (select school_id from school where name = 'Université Laval'), 1, 'maxime', password('maxime'), 'Québec', 'Canada', 'maxime.begin@gmail.com', curdate(), 'Québec', '12345', 1);
+
+
+
+insert into subject (name, language_id) values('Mathématiques', (select language_id from language l where l.short_name='fr'));
+insert into subject (name, language_id) values('Mathematics', (select language_id from language l where l.short_name='en'));
+
+insert into level (name, language_id ) values('Secondaire 1', (select language_id from language l where l.short_name='fr'));
+insert into level (name, language_id ) values('Secondaire 2', (select language_id from language l where l.short_name='fr'));
+insert into level (name, language_id ) values('Secondaire 3', (select language_id from language l where l.short_name='fr'));
+insert into level (name, language_id ) values('Secondaire 4 (416)', (select language_id from language l where l.short_name='fr'));
+insert into level (name, language_id ) values('Secondaire 4 (436)', (select language_id from language l where l.short_name='fr'));
+insert into level (name, language_id ) values('Secondaire 5 (574)', (select language_id from language l where l.short_name='fr'));
+insert into level (name, language_id ) values('Secondaire 5 (536)', (select language_id from language l where l.short_name='fr'));
+
+
+insert into subject_level (subject_id, level_id) 
+	values((select subject_id from subject where name='Mathématiques'),
+		   (select level_id from level where name = "Secondaire 1"));
+
+insert into subject_level (subject_id, level_id) 
+	values((select subject_id from subject where name='Mathématiques'),
+		   (select level_id from level where name = "Secondaire 2"));
+
+insert into subject_level (subject_id, level_id) 
+	values((select subject_id from subject where name='Mathématiques'),
+		   (select level_id from level where name = "Secondaire 3"));
+
+insert into subject_level (subject_id, level_id) 
+	values((select subject_id from subject where name='Mathématiques'),
+		   (select level_id from level where name = "Secondaire 4 (416)"));
+
+insert into subject_level (subject_id, level_id) 
+	values((select subject_id from subject where name='Mathématiques'),
+		   (select level_id from level where name = "Secondaire 4 (436)"));
+
+insert into subject_level (subject_id, level_id) 
+	values((select subject_id from subject where name='Mathématiques'),
+		   (select level_id from level where name = "Secondaire 5 (574)"));
+
+insert into subject_level (subject_id, level_id) 
+	values((select subject_id from subject where name='Mathématiques'),
+		   (select level_id from level where name = "Secondaire 5 (536)"));
