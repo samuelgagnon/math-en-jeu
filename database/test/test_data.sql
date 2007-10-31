4bdb1c873f17a68ad7ae3594b8f1636f028b1e6f
maxime.begin <maxime.begin@00d195c3-d43a-0410-8499-09eb43983aed>
2007-10-31 23:09:10 +0000

    Issue : MEJ-23, MEJ-32
    Minor modification to some primary key (auto increment).

--- database/test/test_data.sql
+++ database/test/test_data.sql
@@ -1,6 +1,7 @@
 truncate table user;
 truncate table school;
 truncate table school_type;
+truncate table role_info;
 truncate table role;
 truncate table subject_level;
 truncate table subject;
@@ -31,6 +32,8 @@ insert into school_type(name, language_id)
 	values ('universitaire',(select language_id from language l where l.short_name='fr'));
 
 
+insert into school(name, school_type_id, city, province, country)
+	values ('none', (select school_type_id from school_type where name = 'universitaire'), 'Québec', 'Québec', 'Canada');
 
 insert into school(name, school_type_id, city, province, country)
 	values('Université Laval', (select school_type_id from school_type where name = 'universitaire'), 'Québec', 'Québec', 'Canada');
@@ -45,15 +48,20 @@ insert into faq(question, awnser, number, language_id)
 
 
 insert into role(tag) values('SIMPLE_USER');
+insert into role_info(role_id, language_id, name, description) values (1,1,'Simple joueur', 'Un simple joueur sans aucun droit');
+insert into role(tag) values('ADMINISTRATOR');
+insert into role_info(role_id, language_id, name, description) values (2,1,'Administrateur', 'Un usager avec des droits d\'administrateur');
 
 
 insert into user (name, last_name, school_id, sexe, username, password, city, country, email, inscription_date, province, email_confirmation_key, role_id)
-	values ('Maxime', 'Bégin', (select school_id from school where name = 'Université Laval'), 1, 'maxime', password('maxime'), 'Québec', 'Canada', 'maxime.begin@gmail.com', curdate(), 'Québec', '12345', 1);
+	values ('Maxime', 'Bégin', (select school_id from school where name = 'Université Laval'), 1, 'maxime', password('maxime'), 'Québec', 'Canada', 'maxime.begin@gmail.com', curdate(), 'Québec', '12345', 2);
 
 
 
-insert into subject (name, language_id) values('Mathématiques', (select language_id from language l where l.short_name='fr'));
+insert into subject (name, language_id) values('Mathématique', (select language_id from language l where l.short_name='fr'));
 insert into subject (name, language_id) values('Mathematics', (select language_id from language l where l.short_name='en'));
+
+insert into subject (name, language_id) values('Chimie', (select language_id from language l where l.short_name='fr'));
 
 insert into level (name, language_id ) values('Secondaire 1', (select language_id from language l where l.short_name='fr'));
 insert into level (name, language_id ) values('Secondaire 2', (select language_id from language l where l.short_name='fr'));
@@ -65,34 +73,42 @@ insert into level (name, language_id ) values('Secondaire 5 (536)', (select lang
 
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématiques'),
+	values((select subject_id from subject where name='Mathématique'),
 		   (select level_id from level where name = "Secondaire 1"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématiques'),
+	values((select subject_id from subject where name='Mathématique'),
 		   (select level_id from level where name = "Secondaire 2"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématiques'),
+	values((select subject_id from subject where name='Mathématique'),
 		   (select level_id from level where name = "Secondaire 3"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématiques'),
+	values((select subject_id from subject where name='Mathématique'),
 		   (select level_id from level where name = "Secondaire 4 (416)"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématiques'),
+	values((select subject_id from subject where name='Mathématique'),
 		   (select level_id from level where name = "Secondaire 4 (436)"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématiques'),
+	values((select subject_id from subject where name='Mathématique'),
 		   (select level_id from level where name = "Secondaire 5 (574)"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématiques'),
+	values((select subject_id from subject where name='Mathématique'),
+		   (select level_id from level where name = "Secondaire 5 (536)"));
+
+
+insert into subject_level (subject_id, level_id) 
+	values((select subject_id from subject where name='Chimie'),
 		   (select level_id from level where name = "Secondaire 5 (536)"));
 
 
 insert into image(user_id, logical_name, physical_name, is_public, description)
     values ((select user_id from user where username='maxime'),
             "test.jpg", "1.jpg", 1, "Une image de test");
+
+
+
