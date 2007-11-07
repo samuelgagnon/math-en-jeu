b3b6d484464f86cf68f267de4d71724917fa848b
maxime.begin <maxime.begin@00d195c3-d43a-0410-8499-09eb43983aed>
2007-11-07 00:25:28 +0000

    Issue : MEJ-23, MEJ-32
    Modification to the subject and category.

--- database/test/test_data.sql
+++ database/test/test_data.sql
@@ -1,13 +1,24 @@
+truncate table image;
+truncate table language; 
+
+
+truncate table question_info;
+truncate table question;
+truncate table answer_type;
+truncate table category;
+truncate table question_info;
+truncate table user_subject_level;
 truncate table user;
-truncate table school;
-truncate table school_type;
+
 truncate table role_info;
 truncate table role;
+truncate table school;
+truncate table school_type;
+
 truncate table subject_level;
 truncate table subject;
 truncate table level;
-truncate table language; 
-truncate table image;
+
 
 insert into language(name, short_name) values('français','fr');
 insert into language(name, short_name) values('english','en');
@@ -57,11 +68,13 @@ insert into user (name, last_name, school_id, sexe, username, password, city, co
 	values ('Maxime', 'Bégin', (select school_id from school where name = 'Université Laval'), 1, 'maxime', password('maxime'), 'Québec', 'Canada', 'maxime.begin@gmail.com', curdate(), 'Québec', '12345', 2);
 
 
+insert into subject values();
 
-insert into subject (name, language_id) values('Mathématique', (select language_id from language l where l.short_name='fr'));
-insert into subject (name, language_id) values('Mathematics', (select language_id from language l where l.short_name='en'));
+insert into subject_info (subject_id, name, language_id) values(1, 'Mathématique', (select language_id from language l where l.short_name='fr'));
+insert into subject_info (subject_id, name, language_id) values(1, 'Mathematics', (select language_id from language l where l.short_name='en'));
 
-insert into subject (name, language_id) values('Chimie', (select language_id from language l where l.short_name='fr'));
+insert into subject values();
+insert into subject_info (subject_id, name, language_id) values(2, 'Chimie', (select language_id from language l where l.short_name='fr'));
 
 insert into level (name, language_id ) values('Secondaire 1', (select language_id from language l where l.short_name='fr'));
 insert into level (name, language_id ) values('Secondaire 2', (select language_id from language l where l.short_name='fr'));
@@ -73,36 +86,36 @@ insert into level (name, language_id ) values('Secondaire 5 (536)', (select lang
 
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématique'),
+	values(1,
 		   (select level_id from level where name = "Secondaire 1"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématique'),
+	values(1,
 		   (select level_id from level where name = "Secondaire 2"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématique'),
+	values(1,
 		   (select level_id from level where name = "Secondaire 3"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématique'),
+	values(1,
 		   (select level_id from level where name = "Secondaire 4 (416)"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématique'),
+	values(1,
 		   (select level_id from level where name = "Secondaire 4 (436)"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématique'),
+	values(1,
 		   (select level_id from level where name = "Secondaire 5 (574)"));
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Mathématique'),
+	values(1,
 		   (select level_id from level where name = "Secondaire 5 (536)"));
 
 
 insert into subject_level (subject_id, level_id) 
-	values((select subject_id from subject where name='Chimie'),
+	values(2,
 		   (select level_id from level where name = "Secondaire 5 (536)"));
 
 
@@ -111,4 +124,33 @@ insert into image(user_id, logical_name, physical_name, is_public, description)
             "test.jpg", "1.jpg", 1, "Une image de test");
 
 
+
+insert into category(subject_id) values(1);
+insert into category_info(category_id, language_id, name, description, subject_id) values (1, 1 , 'Hstoire des math' , 'Catégorie de l\'histoire des math', 1);
+insert into category_info(category_id, language_id, name, description, subject_id) values (1, 2 , 'Math History' , 'Math history category', 1);
+
+
+insert into answer_type(tag) value("MULTIPLE_CHOICE");
+insert into answer_type_info(answer_type_id, language_id, name, description) value(1, 1, 'Choix multiple', 'Question à choix multiple(4)');
+insert into answer_type_info(answer_type_id, language_id, name, description) value(1, 2, 'Multiple choice', 'Multiple choice question ( 4 choices )');
+
+
+insert into question(category_id, answer_type_id) values(1, 1);
+insert into question_info(question_id, creation_date, language_id, question_latex, good_answer, feedback_latex, is_valid, user_id, is_animated) values(1, NOW(), 1, 'Test','test','test', 1, 1, 0);
+insert into question_info(question_id, creation_date, language_id, question_latex, good_answer, feedback_latex, is_valid, user_id, is_animated, question_flash_file) values(1, NOW(), 2, 'Test 2','test 2','test 2', 1, 1, 0, 'test.swf');
+
+insert into comment(question_id, language_id, user_id, datetime, comment ) values(1,1,1,NOW(), "Ceci est un commentaire.");
+insert into comment(question_id, language_id, user_id, datetime, comment ) values(1,1,1,NOW(), "Ceci est un autre commentaire.");
+
+insert into comment(question_id, language_id, user_id, datetime, comment ) values(1,2,1,NOW(), "Ceci est un commentaire.");
+
+
+insert into question_group(language_id, name, description) values (1, 'Groupe test FR', 'Un groupe de test en français');
+insert into question_group(language_id, name, description) values (2, 'Groupe test EN', 'Un groupe de test en anglais');
+insert into question_group(language_id, name, description) values (1, 'Groupe test FR 2', 'Un autre groupe de test en français');
+
+
+insert into question_group_question(question_group_id, question_id, language_id) values(1, 1, 1);
+insert into question_group_question(question_group_id, question_id, language_id) values(2, 1, 2);
+insert into question_group_question(question_group_id, question_id, language_id) values(3, 1, 1);
 
