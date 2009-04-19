049a659e8d9d5d9b3106cdc4a8eb29c12942962e
dbreton76 <dbreton76@00d195c3-d43a-0410-8499-09eb43983aed>
2009-04-19 08:04:01 +0000

    Le fichier est lisible sur unix.

--- admin-tools/library/scripts/bash/tex2swf.sh
+++ admin-tools/library/scripts/bash/tex2swf.sh
@@ -1,44 +1,50 @@
 #!/bin/bash
 
+# first argument : pass the full path of the tex file
+# second argument : the directory to store the swf
+# third argument : pdf for pdf convert, jpeg for jpeg conversion
 
-#first argument : pass the full path of the tex file
-#second argument : the directory to store the swf
-#third argument : pdf for pdf convert, jpeg for jpeg conversion
+olddir=`pwd`
 
-#move the file to the temp directory
-cp -f $1 /tmp
-cd /tmp
-
-#rm -rf convert.log
-
-#touch ./convert.log
-
-echo --- Processing file : $1 ---
-
-ffilename=$1
-filename=${ffilename%.tex}
-
-
-#convert the tex file to dvi
-latex -interaction=batchmode ${ffilename}
-
-#convert the file to eps
-dvips -q -E ${filename}.dvi -o ${filename}.eps
-
-#convert the file to pdf
-if [ "$3" == "pdf" ]
-then
-	#convert the file to pdf then to swf
-	epstopdf ${filename}.eps
-	pdf2swf -q ${filename}.pdf
-else
-	#convert the eps to a jpeg file, then convert to swf
-	convert -density 300x300 -resize 150% ${filename}.eps ${filename}.jpeg
-	jpeg2swf ${filename}.jpeg -o ${filename}.swf
+file_full_path=$1
+tmp_dir=${file_full_path%/*}/
+if [ ${file_full_path}/ == ${tmp_dir} ] ; then 
+    tmp_dir=./
 fi
 
-mv ${filename}.swf $2
+filename=${file_full_path##/*/}
+filename_prefix=${filename%.tex}
 
-echo --- Done creating swf for file : ${ffilename} ---
+cd $tmp_dir
 
+rm -f mej_convert.log
+touch mej_convert.log
 
+echo Changed dir from $olddir to `pwd` >> mej_convert.log
+echo --- Processing file : $1 --- >> mej_convert.log
+
+#convert the tex file to dvi
+#latex doesn't care if the trailing .tex is there or not.
+latex -interaction=batchmode ${filename_prefix} 2>> mej_convert.log
+
+#convert the file to eps
+dvips -q -E ${filename_prefix}.dvi -o ${filename_prefix}.eps 2>> mej_convert.log
+
+#convert the file to swf
+if [ "$3" == "pdf" ]
+then
+    #conversion goes eps->pdf->swf
+    epstopdf ${filename_prefix}.eps 2>> mej_convert.log
+    pdf2swf -q ${filename_prefix}.pdf 2>>mej_convert.log
+else
+    #convertion goes eps->jpg->swf
+    convert -density 300x300 -resize 150\% ${filename_prefix}.eps ${filename_prefix}.jpeg
+    jpeg2swf ${filename_prefix}.jpeg -o ${filename_prefix}.swf
+fi
+
+mv ${filename_prefix}.swf $2
+rm -f ${filename_prefix}.*
+
+echo --- Done creating swf for file : $1 --- >> mej_convert.log
+echo Changing back to $olddir >> mej_convert.log
+cd $olddir
